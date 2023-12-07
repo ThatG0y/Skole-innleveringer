9bbf8612d4771ed1cf066928a8993378eb146f95
ThatG0y <opdahlandreas@gmail.com>
2023-12-07 18:49:33 +0100

    Bug-fixing + litt kommentarer

--- IT-prøve 01.12.2023/main.py
+++ IT-prøve 01.12.2023/main.py
@@ -2,11 +2,30 @@ from modeller.artist import Artist
 from modeller.platealbum.platealbum import Platealbum
 from modeller.platesamling import Platesamling
 
+# alle modell-dokument trenger dokumentasjon
 
-def main() -> None:
+
+def main() -> (
+    None
+):  # Det gjenstår å legge til brukerstyrt opplevelse med bla. input, inntil videre er alt "hardcoded"
+    # Definerer noen objekter
     artist = Artist("Katy", "Perry")
     album = Platealbum("Noe", artist, "ENda mer", 1984)
     samling = Platesamling("adnreas", plater=album)
+
+    # Tester noen funksjoner, rakk ikke lage unit_tests men ville gjort det istedet
+    samling.registrer_artist(Artist("michael", "jackson"))
+    samling.registrer_platealbum(
+        Platealbum("All i want for christmas", Artist("Mariah", "Carrey"), "Noko", 1900)
+    )
+    samling.registrer_platealbum(
+        Platealbum("Last Christmas", Artist("Mariah", "Carrey"), "Noko", 1910)
+    )
+
+    # Gir diverse utskrifter, ville sannsynligvis utbrodert utskrifter mer slik at bruker ikke trenger å lese koden for å forstå hva som skrives ut
+    samling.vis_alle_artistens_plater(Artist("Mariah", "Carrey"))
+    samling.vis_alle_artister()
+    samling.vis_alle_plater()
     print(samling)
 
 
--- IT-prøve 01.12.2023/modeller/platesamling.py
+++ IT-prøve 01.12.2023/modeller/platesamling.py
@@ -2,6 +2,7 @@ from modeller.artist import Artist
 from modeller.platealbum.platealbum import Platealbum
 
 
+# to do: dokumentasjon, korreksjon av protected vs public noen steder (evt laget noen hent_navn() funksjoner osv), og generell opprydding
 class Platesamling:
     def __init__(
         self, eiernavn: str, plater: Platealbum = [], artister: Artist = []
@@ -11,48 +12,65 @@ class Platesamling:
         self._artister = []
         self._artistoversikt = {}
         self._lag_artistoversikt(artister, plater)
+        print(self)
 
     def __str__(self) -> str:
-        print(f"Platesamling:")
+        print(f"Platesamling:\n")
         for artist in self._artister:
             print(artist)
             for plate in self._artistoversikt[artist]:
                 print(plate)
+            print("---------------------------------------------")
         return ""
 
     def _lag_artistoversikt(
         self, artister: Artist = False, plater: Platealbum = False
     ) -> None:
-        if artister:
+        if artister and artister.navn not in [artist.navn for artist in self._artister]:
             self._artister.append(artister)
-            self._artister = list(set(self._artister))
-        if plater:
+        if plater and plater._albumnavn not in [
+            plate._albumnavn for plate in self._plater
+        ]:
             self._plater.append(plater)
-            self._plater = list(set(self._plater))
-            if plater.artist not in self._artister:
+            if plater.artist.navn not in [artist.navn for artist in self._artister]:
                 self._artister.append(plater.artist)
 
         self._artistoversikt = {artist: [] for artist in self._artister}
         for plate in self._plater:
-            if plate.artist in [artist for artist in self._artistoversikt.keys()]:
-                if plate not in [album for album in self._artistoversikt[plate.artist]]:
-                    self._artistoversikt[plate.artist].append(plate)
-            else:
-                self._artistoversikt[plate.artist] = [plate]
-
-        print(self)
+            for artist in self._artistoversikt.keys():
+                if plate.artist.navn == artist.navn:
+                    if plate._albumnavn not in [
+                        album._albumnavn for album in self._artistoversikt[artist]
+                    ]:
+                        self._artistoversikt[artist].append(plate)
 
     def registrer_platealbum(self, platealbum: Platealbum) -> None:
-        self._lag_artistoversikt(plater=[platealbum])
+        self._lag_artistoversikt(plater=platealbum)
 
     def registrer_artist(self, artist: Artist) -> None:
-        self._lag_artistoversikt(plater=[artist])
+        self._lag_artistoversikt(artister=artist)
 
     def vis_alle_plater(self) -> None:
-        print(f"Plater:                   {self._plater}")
+        print("Plater:\n")
+        for plate in self._plater:
+            print(plate)
+            print("---------------------------------------------\n")
 
     def vis_alle_artister(self) -> None:
-        print(f"Artister:                   {self._artister}")
+        print("Artister:\n")
+        for artist in self._artister:
+            print(artist)
+            print("---------------------------------------------\n")
 
     def vis_alle_artistens_plater(self, artist: Artist) -> None:
-        print(f"{artist.navn}:                   {self._artistoversikt[artist]}")
+        for artister in self._artistoversikt.keys():
+            if artist.navn == artister.navn:
+                print(
+                    f"{artister.navn+':':32}{[plate._albumnavn for plate in self._artistoversikt[artister]]}\n"
+                )
+                print("---------------------------------------------\n")
+
+                return None
+        print(
+            f"Ingen artister med navnet '{artist.navn}' er registrert i platesamlingen"
+        )
