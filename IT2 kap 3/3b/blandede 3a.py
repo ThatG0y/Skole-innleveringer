07dbd71525ccaccd1a7beb39e1d672999691e192
ThatG0y <opdahlandreas@gmail.com>
2024-01-16 19:59:46 +0100

    litt pynt

--- IT2 kap 3/3b/blandede 3a.py
+++ IT2 kap 3/3b/blandede 3a.py
@@ -5,10 +5,10 @@ adresse = r"C:\Users\opdah\OneDrive - Akademiet Norge AS\2023-2024 Skolefag\IT\f
 data = pd.read_csv(adresse, sep=";", header=None, index_col=0)
 titles = data.index
 
-fig, ax = plt.subplots(figsize=(15, 5))  # Angir dimensjoner for figure-objektet
+fig, ax = plt.subplots(figsize=(10, 5))  # Angir dimensjoner for figure-objektet
 
-x = data.loc[titles[0]].values
-y = list(map(int, data.loc[titles[1]].values))
+# x = data.loc[titles[0]].values
+# y = list(map(int, data.loc[titles[1]].values))
 
 x_mer = "Over 1000"
 y_mer = sum(list(map(int, data.loc[titles[1]].values))[14:])
@@ -16,7 +16,7 @@ y_mer = sum(list(map(int, data.loc[titles[1]].values))[14:])
 data = data.drop(
     [i for i in range(14, len(data.loc[titles[0]].values) + 1)], axis="columns"
 )
-data[14] = pd.Series(data=[x_mer, y_mer], index=["Meter over havet", "Fritidsbygg"])
+data[14] = pd.Series(data=[x_mer, y_mer], index=titles)
 lengde = len(data.loc[titles[0]].values)
 # data = data.assign(lengde=[x_mer, y_mer])
 
@@ -29,8 +29,14 @@ cmap = plt.get_cmap("viridis")(oppdeling)  # plottets farger
 ax.barh(
     data.loc[titles[0]].values,
     list(map(int, data.loc[titles[1]].values)),
-    label="Importert",
     color=cmap,
 )  # Lager stolpediagram jenter
 
+plt.xlabel(titles[1])
+plt.ylabel(titles[0])
+
+
+plt.title("Høyde over havet for norske fritidsboliger (hytter)", fontsize="15")
+
+plt.grid(axis="x", linestyle="--")
 plt.show()
